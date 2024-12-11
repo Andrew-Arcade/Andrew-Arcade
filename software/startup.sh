3df079a19af23d202bc169151ab6a15e966a891b
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-12-10 21:20:25 -0800

    feat: startup script

--- software/startup.sh
+++ software/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Setup apps as executables.
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/flappyTurd/flappyTurd.x86_64
+
+# Start the driver using box64.
 /usr/local/bin/box64 /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
