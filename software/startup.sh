b02aeab4fbf4286d63262f5626602892dd5d5bba
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-12-10 20:26:01 -0800

    fix: startup.sh

--- software/startup.sh
+++ software/startup.sh
@@ -1,3 +1,5 @@
-usr/bin/chmod +x apps/driver/driver.x86_64
-usr/bin/chmod +x apps/flappyTurd/flappyTurd.x86_64
-usr/local/bin/box64 apps/driver/driver.x86_64
+#!/bin/bash
+
+/usr/bin/chmod +x apps/driver/driver.x86_64
+/usr/bin/chmod +x apps/flappyTurd/flappyTurd.x86_64
+/usr/local/bin/box64 apps/driver/driver.x86_64
