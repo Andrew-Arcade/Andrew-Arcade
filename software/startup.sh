da5b89daac191f793b84c59cdbc7a800afb07f59
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-02-01 19:34:49 -0800

    upt(software): startup.sh

--- software/startup.sh
+++ software/startup.sh
@@ -3,6 +3,7 @@
 # Setup apps as executables.
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/flappyTurd/flappyTurd.x86_64
+/usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/schoolCountdown/schoolCountdown.x86_64
 
 # Start the driver using box64.
 /usr/local/bin/box64 /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
