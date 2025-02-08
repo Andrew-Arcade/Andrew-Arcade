94953d9fe1d7a34282f82614ececb8cc9c222e6f
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-02-07 22:16:02 -0800

    upt(software): add oranyth to startup script

--- software/startup.sh
+++ software/startup.sh
@@ -4,6 +4,7 @@
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/flappyTurd/flappyTurd.x86_64
 /usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/schoolCountdown/schoolCountdown.x86_64
+/usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/oranyth/oranyth.x86_64
 
 # Start the driver using box64.
 /usr/local/bin/box64 /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
