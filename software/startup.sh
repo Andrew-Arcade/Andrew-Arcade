e9713ee196d5455c15f3dd4640a1aaaa7798603b
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-02-27 21:57:09 -0800

    upt(software): driver

--- software/startup.sh
+++ software/startup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Setup doom files.
+
+
 # Setup apps as executables.
-/usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
-/usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/flappyTurd/flappyTurd.x86_64
-/usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/schoolCountdown/schoolCountdown.x86_64
-/usr/bin/chmod +x /home/andrew/Andrew-Arcade/software/apps/oranyth/oranyth.x86_64
+chmod +x /home/$USER/Andrew-Arcade/software/apps/driver/driver.x86_64
+chmod +x /home/$USER/Andrew-Arcade/software/apps/flappyTurd/flappyTurd.x86_64
+chmod +x /home/$USER/Andrew-Arcade/software/apps/schoolCountdown/schoolCountdown.x86_64
+chmod +x /home/$USER/Andrew-Arcade/software/apps/oranyth/oranyth.x86_64
 
 # Start the driver using box64.
-/usr/local/bin/box64 /home/andrew/Andrew-Arcade/software/apps/driver/driver.x86_64
+box64 /home/$USER/Andrew-Arcade/software/apps/driver/driver.x86_64
