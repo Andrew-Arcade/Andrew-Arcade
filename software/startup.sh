cb7541be517deee2e403990b3d7313b99fe0a3ea
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-10-03 09:28:33 -0700

    rmv: software - moved to seperate repo

--- software/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Automatically find and make all .x86_64 files in apps/ executable.
-find /home/$USER/Andrew-Arcade/software/apps/ -type f -name "*.x86_64" -exec chmod +x {} \;
-
-# Start the driver using box64.
-box64 /home/$USER/Andrew-Arcade/software/apps/driver/driver.x86_64
